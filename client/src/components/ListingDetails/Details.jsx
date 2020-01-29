0cee7624f8853805054cac44b779d97c8759b930
Sager Williams <sagerwilliams0011@gmail.com>
2020-01-29 17:16:24 -0600

    Completed basic details component

--- client/src/components/ListingDetails/Details.jsx
+++ client/src/components/ListingDetails/Details.jsx
@@ -12,11 +12,9 @@ export default class Details extends React.Component {
     return (
       <div className="details">
         <div className={this.state.expanded ? "details-div-expanded" : "details-div-hidden"}>
-          <span>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius voluptatem possimus perspiciatis maxime accusamus, hic odio tempore, voluptate fugiat impedit rerum harum. Vero qui officia pariatur sit quaerat facere veniam? Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam consectetur laboriosam architecto nulla corporis consequatur iure porro possimus quibusdam maxime aliquam accusamus at sunt, soluta voluptates inventore, sint temporibus quis! Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae pariatur illo ipsam vero necessitatibus possimus porro, numquam, tempore suscipit officiis voluptate veritatis in consectetur dicta tenetur ut molestias voluptates animi! Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem totam itaque alias mollitia saepe soluta recusandae in expedita non eius error, atque quae labore, sapiente dolore maiores, molestias corporis! Nemo.
-          </span>
+          <span>{this.props.description}</span>
         </div>
-        <button className="details-expand" onClick={this.setState({expanded: true})}>Learn more about this item</button>
+        <button className="details-expand" onClick={() => {this.state.expanded ? this.setState({expanded: false}) : this.setState({expanded: true})}}>{this.state.expanded ? "Less" : "Learn more about this item"}</button>
       </div>
     )
   }
--- client/src/components/ListingDetails/ListingDetails.jsx
+++ client/src/components/ListingDetails/ListingDetails.jsx
@@ -13,7 +13,7 @@ export default class ListingDetails extends React.Component {
         <div className="listing-details">
             <h3><strong>Item Details</strong></h3>
             <Handmade />
-            <Details />
+            <Details description={"This is a test description, please disreguard :) asdasdasdkjasbfskjdbfs sdkjfbskjdfnbs fsd fshd fksjhd fkdfn gdkhfbgdkfbgd kfng dkfn gkdfn gkdnf gkdnf gkdn fgkdnfg kjnd fgkndf gkdn fgknd fgknd fgkd fgknd fgkdnf gkdnf gkdnf gkn dfkn gd Thanks :)This is a test description, please disreguard :) asdasdasdkjasbfskjdbfs sdkjfbskjdfnbs fsd fshd fksjhd fkdfn gdkhfbgdkfbgd kfng dkfn gkdfn gkdnf gkdnf gkdn fgkdnfg kjnd fgkndf gkdn fgknd fgknd fgkd fgknd fgkdnf gkdnf gkdnf gkn dfkn gd Thanks :)This is a test description, please disreguard :) asdasdasdkjasbfskjdbfs sdkjfbskjdfnbs fsd fshd fksjhd fkdfn gdkhfbgdkfbgd kfng dkfn gkdfn gkdnf gkdnf gkdn fgkdnfg kjnd fgkndf gkdn fgknd fgknd fgkd fgknd fgkdnf gkdnf gkdnf gkn dfkn gd Thanks :)This is a test description, please disreguard :) asdasdasdkjasbfskjdbfs sdkjfbskjdfnbs fsd fshd fksjhd fkdfn gdkhfbgdkfbgd kfng dkfn gkdfn gkdnf gkdnf gkdn fgkdnfg kjnd fgkndf gkdn fgknd fgknd fgkd fgknd fgkdnf gkdnf gkdnf gkn dfkn gd Thanks :)"}/>
         </div>
     )
   }
