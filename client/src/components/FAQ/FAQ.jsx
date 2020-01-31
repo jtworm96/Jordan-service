7f7097dd91614235b31d74dc46b0f8b5c5d70c91
Sager Williams <sagerwilliams0011@gmail.com>
2020-01-31 15:43:03 -0600

    Completed basic faq component

--- client/src/components/FAQ/FAQ.jsx
+++ client/src/components/FAQ/FAQ.jsx
@@ -3,13 +3,25 @@ import React from 'react';
 export default class FAQ extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      expanded: false
+    };
+    this.toggleFAQ = this.toggleFAQ.bind(this);
+  }
+
+  toggleFAQ() {
+    this.setState({expanded: !this.state.expanded});
   }
 
   render () {
     return (
         <div className="FAQ">
-            FAQ
+            <button className="faqbutton" onClick={() => {this.toggleFAQ()}}>FAQ</button>
+            <div className={this.state.expanded ? "faqexpanded" : "faqclosed"}>
+              Q: Who worked on this right column?
+              <br/>
+              A: <a className="faqlink" href="https://github.com/Etsy-FEC/Checkout-Description/blob/master/README.md" target="_blank">Sager Williams</a>
+            </div>
         </div>
     )
   }
